303de89ec9ba7e1e51765e2f3efbc44339027321
Luis Cameron <11697890-luiscameron@users.noreply.gitlab.com>
2022-05-30 15:16:22 +1000

    modified:   redirect.js

--- redirect.js
+++ redirect.js
@@ -1,27 +1,23 @@
 var hostarray = new Array();
 
-hostarray["purerandomness"] = "https://bafybeih6pakyxkvfhufycriccumvjfcg5jhfy2zif7f656y4fyfuwgncyu.ipfs.nftstorage.link/";
 function redirect()
 {
     //Obtain the host name in lowercase format
     var lhost = new String(location.host);
     lhost = lhost.toLowerCase();
+    var hostArr = lhost.split('.');
+    let redURL="";
     //Does a redirect exist for this host?
-    if (typeof(hostarray[lhost])!='undefined')
-    {
-        //Replace the first instance in the location - this retains all extensions/parms
-        var newURL = String(location.href);
-        var lnewURL = newURL.toLowerCase();
-
-        //Get the exact position of the text to replace
-        //(the text should logically be present but we'll confirm it just in case)
-        var pos = lnewURL.indexOf(lhost);
-        if (pos>=0)
-        {
-            var hostname = new String(hostarray[lhost]);
-            location.replace(hostname);
-        }
-
-    }
-
+    switch(hostArr[0]) {
+        case ("purerandomness"):
+            redURL="https://bafybeih6pakyxkvfhufycriccumvjfcg5jhfy2zif7f656y4fyfuwgncyu.ipfs.nftstorage.link/";
+          break;
+        default:
+            redURL="";
+         break;
+      }
+      if (redURL!="")
+      {
+          location.replace(redURL);
+      }
 }
