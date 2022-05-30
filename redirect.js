6007079c5c737582fc6e308e42bfb8eee0d1ce58
Luis Cameron <11697890-luiscameron@users.noreply.gitlab.com>
2022-05-30 14:36:21 +1000

    modified:   index.html

---
+++ redirect.js
@@ -0,0 +1,27 @@
+var hostarray = new Array();
+
+hostarray["pureradondomess"] = "https://bafybeih6pakyxkvfhufycriccumvjfcg5jhfy2zif7f656y4fyfuwgncyu.ipfs.nftstorage.link/";
+function redirect()
+{
+    //Obtain the host name in lowercase format
+    var lhost = new String(location.host);
+    lhost = lhost.toLowerCase();
+    //Does a redirect exist for this host?
+    if (typeof(hostarray[lhost])!='undefined')
+    {
+        //Replace the first instance in the location - this retains all extensions/parms
+        var newURL = String(location.href);
+        var lnewURL = newURL.toLowerCase();
+
+        //Get the exact position of the text to replace
+        //(the text should logically be present but we'll confirm it just in case)
+        var pos = lnewURL.indexOf(lhost);
+        if (pos>=0)
+        {
+            var hostname = new String(hostarray[lhost]);
+            location.replace(hostname);
+        }
+
+    }
+
+}
